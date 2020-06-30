70fc6168821398ba334b852676f2cf8f48e25d04
Junyi Ji <junyiji@gmail.com>
2020-06-30 09:31:01 +0200

    Add a functions to get a random number and to show a hint

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 'use strict';
+// Variables
+let inputNumberEl = document.querySelector('.js-input');
+const button = document.querySelector('.js-button');
+let hint = document.querySelector('.js-hint');
+let attemps = document.querySelector('.js-attemps')
+
+// Generar número aleatorio
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+let randomNumber = getRandomNumber(100);
+
+console.log(randomNumber)
+
+// Mostrar pista
+function showHint() {
+    if (inputNumberEl.value > randomNumber) {
+        hint.innerHTML = 'Demasiado alto'
+    } else if (inputNumberEl.value < randomNumber) {
+        hint.innerHTML = 'Demasiado bajo'
+    } else {
+        hint.innerHTML = 'Has ganado campeona!!!'
+    }
+}
+
+function showAttemps() {
+
+}
+
+function showMessages() {
+    showHint();
+    showAttemps();
+}
+
+button.addEventListener('click', showMessages);
