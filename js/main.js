98daf1ef8fa31b55484c68b51e3cc1b2b8e79ad9
Junyi Ji <junyiji@gmail.com>
2020-06-30 09:54:56 +0200

    Add attemps

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 let inputNumberEl = document.querySelector('.js-input');
 const button = document.querySelector('.js-button');
 let hint = document.querySelector('.js-hint');
-let attemps = document.querySelector('.js-attemps')
+let attempsEl = document.querySelector('.js-attemps');
+let attemps = 0;
 
 // Generar número aleatorio
 function getRandomNumber(max) {
@@ -14,21 +15,25 @@ let randomNumber = getRandomNumber(100);
 
 console.log(randomNumber)
 
-// Mostrar pista
+// Mostrar mensaje con pista
 function showHint() {
     if (inputNumberEl.value > randomNumber) {
-        hint.innerHTML = 'Demasiado alto'
+        hint.innerHTML = 'Demasiado alto';
+        attemps += 1;
     } else if (inputNumberEl.value < randomNumber) {
-        hint.innerHTML = 'Demasiado bajo'
+        hint.innerHTML = 'Demasiado bajo';
+        attemps += 1;
     } else {
         hint.innerHTML = 'Has ganado campeona!!!'
     }
 }
 
+// Mostrar mensaje con número de intentos
 function showAttemps() {
-
+    attempsEl.innerHTML = `Número de intentos: ${attemps}`
 }
 
+// Handler
 function showMessages() {
     showHint();
     showAttemps();
